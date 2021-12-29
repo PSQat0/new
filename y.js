fa46167f167c194d5671c2e2e7ff6197d43ba133
unknown <sobek9142@gmail.com>
2021-12-29 21:17:46 +0100

    .

--- y.js
+++ y.js
@@ -1,13 +1,28 @@
 let container=document.getElementById("container");
+
+//function creates div squares
 function createSquare(){
     let addedDiv=document.createElement("div");
     addedDiv.style.backgroundColor='red';
-    addedDiv.style.maxWidth="5vw";
-    addedDiv.style.height="5vw";
+    // addedDiv.style.maxWidth="5vw";
+    // addedDiv.style.height="5vw";
+    addedDiv.style.margin='0';
+    addedDiv.textContent="123";
+    addedDiv.style.textAlign="center";
+    
     return addedDiv;
 }
-function createCanvas(rows){
-    for(let i=0; i<rows; i++){
-        container.appendChild(createSquare());
-    }
+//function creates a grid of divs with specified amount of rows and columns 
+function createCanvas(rows, columns){
+    container.style.setProperty(
+        'grid-template-columns',`repeat(${columns},1fr)`
+    );
+     for(let i=0; i<columns; i++){
+         for(let j=0; j<rows; j++){
+             container.appendChild(createSquare());
+         }
+     }
+
 }
+createCanvas(15,15);
+
