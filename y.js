ad2c6fea4fd84d5074d01c1b27817c2511f41b12
unknown <sobek9142@gmail.com>
2021-12-31 09:53:54 +0100

    y.js

--- y.js
+++ y.js
@@ -4,10 +4,11 @@ let container=document.getElementById("container");
 function createSquare(){
     let addedDiv=document.createElement("div");
     addedDiv.style.backgroundColor='red';
-    // addedDiv.style.maxWidth="5vw";
-    // addedDiv.style.height="5vw";
+    
     addedDiv.style.margin='0';
-    addedDiv.textContent="123";
+    addedDiv.style.height="20px";
+    addedDiv.style.maxWidth="92px";
+    
     addedDiv.style.textAlign="center";
     
     return addedDiv;
